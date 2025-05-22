b853ab019b1a870c7c9ca6657fdade81422d4f53
SchrodingersKage <DaxKHarris@gmail.com>
2025-05-22 14:54:07 -0600

    Added comments for readability

--- bash-scripts/backup_home.sh
+++ bash-scripts/backup_home.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+
+# === Initial setup ===
 user = $1
 backup_root="/backups/$user"
 timestamp=$(date +%Y-%m-%d_%H-%M-%S)
 backup_file="$backup_root/home_${user}_$timestamp.tar.gz"
+home_dir="/home/$user"
 
+# === Error handling ===
 if [[ -z "$user" ]]; then
     echo "Empty user var"
     echo "Usage $0 username"
@@ -15,12 +19,12 @@ if ! id "$user" &>/dev/null; then
     exit 1
 fi
 
-home_dir="/home/$user"
 if [[ ! -d "$home_dir" ]]; then
     echo "Home directory $home_dir does not exist."
     exit 1
 fi
 
+# === Make directory and backup ===
 mkdir -p "$backup_root"
 
 tar -czf "$backup_file" "$home_dir"
